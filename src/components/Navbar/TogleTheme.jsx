9bd8162874184dadcb5b302d4677e143176d31f1
Fahrur Rozi <fahrurrozai587@gmail.com>
2023-10-22 19:09:41 +0700

    toggle

--- src/components/Navbar/TogleTheme.jsx
+++ src/components/Navbar/TogleTheme.jsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { BsFillMoonFill, BsFillSunFill } from "react-icons/bs";
+import {
+  BsFillMoonFill,
+  BsFillSunFill,
+  BsMoonFill,
+  BsSunFill,
+} from "react-icons/bs";
 
 const TogleTheme = (props) => {
   const [theme, setChange] = useState(sessionStorage.getItem("theme"));
@@ -8,35 +13,40 @@ const TogleTheme = (props) => {
     if (theme === true || theme === "true") {
       document.documentElement.classList.add("dark");
       sessionStorage.setItem("theme", true);
-      setChange(true)
+      setChange(true);
     } else if (theme === false || theme === "false") {
       document.documentElement.classList.remove("dark");
       sessionStorage.setItem("theme", false);
-      setChange(false)
+      setChange(false);
     } else {
       setChange(window.matchMedia("(prefers-color-scheme: dark)").matches);
     }
   }, [theme]);
 
   return (
-    <div id="theme" className={"w-full sm:w-auto flex justify-end"}>
-      <label className="flex items-center relative w-max cursor-pointer select-none">
+    <div
+      id="theme"
+      className={"w-full sm:w-auto flex justify-end items-center"}
+    >
+      <label
+        htmlFor="toggle"
+        className="relative w-[43px] focus-within:ring-2 ring-slate-50 rounded-full flex overflow-hidden cursor-pointer"
+      >
         <input
           type="checkbox"
+          id="toggle"
           checked={theme ? true : false}
           onChange={() => setChange(!theme)}
           onKeyUp={(e) => {
             if (e.key === "Enter") setChange(!theme);
           }}
-          className="peer bg-white appearance-none cursor-pointer w-10 h-5 rounded-full"
+          className="peer appearance-none w-full h-6 m-0 bg-blue-300 checked:bg-slate-600 duration-300"
         />
-        <span className="absolute left-1">
-          <BsFillMoonFill className="w-3 h-3" fill="black" />
-        </span>
-        <span className="absolute right-1">
-          <BsFillSunFill className="w-3 h-3" fill="black" />
-        </span>
-        <span className="w-4 h-4 peer-checked:translate-x-4 right-5 absolute rounded-full transform transition-transform bg-blue-400 dark:bg-gray-500" />
+        <span className="absolute w-4 h-4 peer-checked:translate-x-[19px] z-10 shadow-lg duration-300 bg-slate-50 bottom-1 left-1 rounded-full"></span>
+        <div className="flex px-0.5 delay-150 -left-[23px] peer-checked:translate-x-[25px] duration-300 items-center justify-between w-16 h-full absolute top-0">
+          <BsMoonFill className="w-4 h-4 peer fill-yellow-100" />
+          <BsSunFill className="w-4 h-4 fill-yellow-200" />
+        </div>
       </label>
     </div>
   );
